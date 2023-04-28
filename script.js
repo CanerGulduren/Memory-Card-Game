3cd8967371a457c29e02a3b65b7103ca8d78d2ce
CanerGulduren <dngnhntr@gmail.com>
2023-04-28 12:10:11 +0300

    Decrease attempt count
    Change difficulty and decrease attempt count

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-let isGameActive = true;
+let isGameActive;
 let isCardMatched;
 const cards = Array.from(document.querySelectorAll(".card"));
 const imageElem = Array.from(document.querySelectorAll(".gameboard img"));
 const questionImg = "img/question.png";
+const gameEndScreen = document.querySelector(".game-end");
+const gameEndMessage = document.querySelector(".end-message");
+let difficultyChange = document.querySelector(".difficulty-change");
+let selectedDifficulty =
+  document.getElementsByTagName("option")[difficultyChange.selectedIndex].value;
+let remainAttempt = document.querySelector(".remain-attempt");
 let cardSource = [
   "img/apple.png",
   "img/banana.png",
@@ -16,28 +22,11 @@ let cardSource = [
 let CARD_DATA = [...cardSource, ...cardSource].sort(() => Math.random() - 0.5);
 let matchedCards = [];
 
-getSrcData();
-
-cards.forEach((card) => {
-  let cardImg = card.firstElementChild;
-  card.addEventListener("click", () => {
-    if (matchedCards.length > 1) {
-      return;
-    }
-    if (card.classList.contains("flip-card")){
-      return;
-    }
-    card.classList.add("flip-card", "flipped");
-    cardImg.src = cardImg.dataset.src;
-    checkCardMatch(cardImg);
-    setCardBehavior();
-  });
-});
-
 function getSrcData() {
   for (let i = 0; i < CARD_DATA.length; i++) {
     imageElem[i].dataset.src = CARD_DATA[i];
   }
+  remainAttempt.innerText = selectedDifficulty;
 }
 
 function checkCardMatch(img) {
@@ -47,11 +36,16 @@ function checkCardMatch(img) {
     : (isCardMatched = false);
 }
 
-function setCardBehavior() {
+function setRules() {
   let flipCard = document.querySelectorAll(".flipped");
   if (flipCard.length < 2) {
     return;
   }
+  cardBehaviour(flipCard);
+  decreaseAttempt();
+}
+
+function cardBehaviour(flipCard) {
   flipCard.forEach((card) => {
     if (isCardMatched) {
       card.style.backgroundColor = "#84ff46";
@@ -70,3 +64,45 @@ function flipAfterAnim(card) {
   card.firstElementChild.src = questionImg;
   matchedCards.length = 0;
 }
+
+function decreaseAttempt() {
+  if (isCardMatched) {
+    return;
+  }
+  let currentAttempt = Number(remainAttempt.innerText);
+  currentAttempt--;
+  remainAttempt.innerText = currentAttempt;
+  if (currentAttempt === 0) {
+    isGameActive = false;
+  }
+}
+
+function disableDiffChange() {
+  isGameActive
+    ? (difficultyChange.disabled = true)
+    : (difficultyChange.disabled = false);
+}
+
+getSrcData();
+
+cards.forEach((card) => {
+  let cardImg = card.firstElementChild;
+  card.addEventListener("click", () => {
+    isGameActive = true;
+    if (matchedCards.length > 1 || card.classList.contains("flip-card")) {
+      return;
+    }
+    card.classList.add("flip-card", "flipped");
+    cardImg.src = cardImg.dataset.src;
+    checkCardMatch(cardImg);
+    setRules();
+    disableDiffChange();
+  });
+});
+
+difficultyChange.addEventListener("change", () => {
+  remainAttempt.innerText =
+    document.getElementsByTagName("option")[
+      difficultyChange.selectedIndex
+    ].value;
+});
